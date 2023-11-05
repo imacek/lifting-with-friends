877e9f067a6338a4de3b1ac0cafd0f6dbf1a7a20
Ivan Macek <imacek@riotgames.com>
2023-11-05 15:36:50 -0800

    showing big 5 excercises and built a selector for metric type

--- client/js/dashboard.js
+++ client/js/dashboard.js
@@ -3,6 +3,95 @@
 (() => {
   'use strict'
 
+  let data = null;
+  let selectedMetric = null;
+
+  const metricSelectBtnElems = document.getElementsByClassName('metric-select-btn');
+
+  function handleMetricSelectButtonClicked(clickedBtnElem) {
+    selectedMetric = clickedBtnElem.getAttribute('metric')
+
+    Array.from(metricSelectBtnElems).forEach(element => {
+      if (element.getAttribute('metric') === selectedMetric) {
+        element.classList.add('btn-primary')
+        element.classList.remove('btn-outline-secondary')
+      } else {
+        element.classList.add('btn-outline-secondary')
+        element.classList.remove('btn-primary')
+      }
+    });
+
+    reloadCharts()
+  }
+
+  handleMetricSelectButtonClicked(Array.from(metricSelectBtnElems)[0]);
+
+  Array.from(metricSelectBtnElems).forEach(element => {
+    element.onclick = event => handleMetricSelectButtonClicked(event.target)
+  })
+
+  function deleteCharts() {
+    Array.from(document.getElementsByClassName('exercise-chart')).forEach(element => {
+      element.remove();
+    });
+  }
+
+  function showChart(exercise, metric, data) {
+
+    const canvasElem = document.createElement("canvas");
+    canvasElem.className = 'exercise-chart my-4 w-100'
+    canvasElem.width = 900
+    canvasElem.height = 380
+
+    const mainElem = document.getElementsByTagName('main')[0];
+    mainElem.insertAdjacentElement('beforeend', canvasElem)
+
+    const datasets = Object.entries(data).map(([user, userData]) => {
+      return { label: user, data: userData[exercise] }
+    })
+
+    new Chart(canvasElem, {
+      type: 'line',
+      data: {
+        datasets: datasets
+      },
+      options: {
+        scales: {
+          x: {
+            type: 'time',
+          }
+        },
+        parsing: {
+          xAxisKey: 'timestamp',
+          yAxisKey: metric
+        },
+        plugins: {
+          title: {
+            display: true,
+            text: exercise,
+          },
+          legend: {
+            display: true
+          }
+        }
+      }
+    })
+  }
+
+  function reloadCharts() {
+    if (!data) {
+      return
+    }
+
+    deleteCharts()
+    showChart('Squat (Barbell)', selectedMetric, data)
+    showChart('Deadlift (Barbell)', selectedMetric, data)
+    showChart('Bench Press (Barbell)', selectedMetric, data)
+    showChart('Overhead Press (Barbell)', selectedMetric, data)
+    showChart('Bent Over Row (Barbell)', selectedMetric, data)
+  }
+
+
   fetch('/api/data', {
     method: 'GET',
     headers: {
@@ -11,37 +100,8 @@
   })
     .then(response => response.json())
     .then(response => {
-      new Chart(document.getElementById('exerciseChart'), {
-        type: 'line',
-        data: {
-          datasets: [
-            { label: 'ivan', data: response['ivan']['Deadlift (Barbell)'] },
-            { label: 'vinko',  data: response['vinko']['Deadlift (Barbell)'] },
-            { label: 'linda', data: response['linda']['Deadlift (Barbell)'] },
-            { label: 'yomach', data: response['yomach']['Deadlift (Barbell)'] },
-          ]
-        },
-        options: {
-          scales: {
-            x: {
-              type: 'time',
-            }
-          },
-          parsing: {
-            xAxisKey: 'timestamp',
-            yAxisKey: 'maxOneRepMax'
-          },
-          plugins: {
-            legend: {
-              display: true
-            },
-            tooltip: {
-              boxPadding: 3
-            }
-          }
-        }
-      })
-
+      data = response
+      reloadCharts()
     })
 
 })()
