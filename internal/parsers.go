d32afc792df6a9dd91aa5fca8a6d95a96a06e59f
Ivan Macek <imacek@riotgames.com>
2023-11-11 16:52:14 -0800

    fixed a bug with skipping first line

--- internal/parsers.go
+++ internal/parsers.go
@@ -10,10 +10,9 @@ import (
 // Expected Strong Apple input headers:
 // Date,Workout Name,Duration,Exercise Name,Set Order,Weight,Reps,Distance,Seconds,Notes,Workout Notes,RPE
 func parseAppleStrongCsvRecords(records [][]string) ([]LiftingSet, error) {
-	noHeaderRecords := records[1:]
-	cleanRecords := make([]LiftingSet, len(noHeaderRecords))
+	cleanRecords := make([]LiftingSet, len(records))
 
-	for index, record := range noHeaderRecords {
+	for index, record := range records {
 		time, err := time.ParseInLocation("2006-01-02 15:04:05", record[0], losAngelesLocation)
 		if err != nil {
 			log.Println(fmt.Sprintf("Parsing Time failed at row %d", index))
@@ -49,10 +48,9 @@ func parseAppleStrongCsvRecords(records [][]string) ([]LiftingSet, error) {
 // Expected Strong Android input headers:
 // Date;Workout Name;Exercise Name;Set Order;Weight;Weight Unit;Reps;RPE;Distance;Distance Unit;Seconds;Notes;Workout Notes;Workout Duration
 func parseAndroidStrongCsvRecords(records [][]string) ([]LiftingSet, error) {
-	noHeaderRecords := records[1:]
-	cleanRecords := make([]LiftingSet, len(noHeaderRecords))
+	cleanRecords := make([]LiftingSet, len(records))
 
-	for index, record := range noHeaderRecords {
+	for index, record := range records {
 		time, err := time.ParseInLocation("2006-01-02 15:04:05", record[0], losAngelesLocation)
 		if err != nil {
 			log.Println(fmt.Sprintf("Parsing Time failed at row %d", index))
@@ -88,10 +86,9 @@ func parseAndroidStrongCsvRecords(records [][]string) ([]LiftingSet, error) {
 // Expected DailyStrength Android input headers:
 // "Date","Workout name","Exercise","Set","Weight","Reps","Distance","Duration","Measurement unit","Notes"
 func parseDailyStrengthAndroidCsvRecords(records [][]string) ([]LiftingSet, error) {
-	noHeaderRecords := records[1:]
-	cleanRecords := make([]LiftingSet, len(noHeaderRecords))
+	cleanRecords := make([]LiftingSet, len(records))
 
-	for index, record := range noHeaderRecords {
+	for index, record := range records {
 		time, err := time.ParseInLocation("2006-01-02 15:04:05", record[0], losAngelesLocation)
 		if err != nil {
 			log.Println(fmt.Sprintf("Parsing Time failed at row %d", index))
